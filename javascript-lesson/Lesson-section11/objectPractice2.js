3ecc1a0751b8529ffc6ebeb03b791fab64c4b0ed
Henry Chou <henry.chou714@gmail.com>
2020-07-09 21:18:26 -0700

    object practice excersise 7

--- javascript-lesson/Lesson-section11/objectPractice2.js
+++ javascript-lesson/Lesson-section11/objectPractice2.js
@@ -1,25 +1,31 @@
 var loginCheck = true;
+var accountCreationCheck = false;
 
 var login = function(userInput1,userInput2){
     if (userInput1 === user1.userName && userInput2 === user1.password ){
-
-        return prompt("Welcome! "+user1.userName);
+        console.log("user logged in");
+        return alert("Welcome! "+user1.userName);
 
     }else if(userInput1 === user2.userName && userInput2 === user2.password){
-
-        return prompt("Welcome! "+user2.userName);
+        console.log("user logged in");
+        return alert("Welcome! "+user2.userName);
 
     }else if(userInput1 === user3.userName && userInput2 === user3.password){
-
-        return prompt("Welcome! "+user3.userName);
+        console.log("user logged in");
+        return alert("Welcome! "+user3.userName);
 
     }else{
-
-        return alert("Sorry, you don't have an account, let's create one!");
-        var newUserName = prompt("what is your desired username?: ");
-        var newPassword = prompt("what is your desired username?: ");
+        console.log("user FAILED to log in");
+        return alert("Sorry, that was incorrect!");
     }
-    database.push(user4.newUserName, user4.newPassword)
+}
+
+var accountCreation = function (){
+    var newUserName = prompt("what is your desired username?: ");
+    var newPassword = prompt("what is your desired password?: ");
+    //creates a new user object
+    database.push(user4);
+
 }
 
 var user1 = {
@@ -30,22 +36,31 @@ var user1 = {
 
 var user2 = {
     userName: "joe",
-    password: "789"
+    password: "456"
 };
 
 var user3 = {
     userName: "sally",
-    password: "456"
+    password: "789"
 };
 
 var database = [user1, user2, user3];
 
-while(loginCHeck === true){
+while(loginCheck === true || accountCreationCheck === false){
 
     var userInput1 = prompt("What is your username?: ");
     var userInput2 = prompt("What is your password?: ");
     login(userInput1, userInput2)
     loginCheck = false;
+    console.log("login attempts expired")
+    var userChoice = prompt("Would you like to create a new account? (yes/no): ");
 
+    if (userChoice === "yes"){
+        accountCreation();
+        accountCreationCheck = true;
+    }else{
+        console.log("user did not want to create a new account");
+        accountCreationCheck = true;
+    }   
 }
-
+    
