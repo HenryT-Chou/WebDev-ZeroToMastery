a6c21b29561c7c191ce2f53cf412d76aae7fd44f
Henry Chou <henry.chou714@gmail.com>
2020-07-10 11:23:05 -0700

    extended excersise 7 and created a a way to login and create a new user

--- javascript-lesson/Lesson-section11/objectPractice2.js
+++ javascript-lesson/Lesson-section11/objectPractice2.js
@@ -1,18 +1,61 @@
 var loginCheck = true;
 var accountCreationCheck = false;
 
+var user1 = {
+    userName: "bob",
+    password: "123",
+    
+};
+
+var user2 = {
+    userName: "joe",
+    password: "456",
+    
+};
+
+var user3 = {
+    userName: "sally",
+    password: "789",
+  
+};
+
+var database = [user1, user2, user3];
+var newsFeed = [
+    {
+        userName: "bob",
+        timeline: "I'm loving JS"
+    },
+    {
+        userName: "joe",
+        timeline: "how is time moving so slowly",
+    },
+    {
+        userName: "sally",
+        timeline: "can't wait for the weekend",
+    }
+];
+
 var login = function(userInput1,userInput2){
     if (userInput1 === user1.userName && userInput2 === user1.password ){
+        accountCreationCheck = true
         console.log("user logged in");
-        return alert("Welcome! "+user1.userName);
-
+        alert("Welcome! "+user1.userName);
+        console.log(newsFeed);
+        return 
+        
     }else if(userInput1 === user2.userName && userInput2 === user2.password){
+        accountCreationCheck = true
         console.log("user logged in");
-        return alert("Welcome! "+user2.userName);
-
+        alert("Welcome! "+user2.userName);
+        console.log(newsFeed);
+        return
+        
     }else if(userInput1 === user3.userName && userInput2 === user3.password){
+        accountCreationCheck = true
         console.log("user logged in");
-        return alert("Welcome! "+user3.userName);
+        alert("Welcome! "+user3.userName);
+        console.log(newsFeed);
+        return
 
     }else{
         console.log("user FAILED to log in");
@@ -21,30 +64,19 @@ var login = function(userInput1,userInput2){
 }
 
 var accountCreation = function (){
+
     var newUserName = prompt("what is your desired username?: ");
     var newPassword = prompt("what is your desired password?: ");
     //creates a new user object
+    var user4 = {userName: newUserName,password:newPassword};
     database.push(user4);
 
+    console.log("new account created");
+    alert("Welcome! "+database[3].userName);
+    console.log(newsFeed);        
+    return 
+
 }
-
-var user1 = {
-    userName: "bob",
-    password: "123"
-
-};
-
-var user2 = {
-    userName: "joe",
-    password: "456"
-};
-
-var user3 = {
-    userName: "sally",
-    password: "789"
-};
-
-var database = [user1, user2, user3];
 
 while(loginCheck === true || accountCreationCheck === false){
 
@@ -53,14 +85,18 @@ while(loginCheck === true || accountCreationCheck === false){
     login(userInput1, userInput2)
     loginCheck = false;
     console.log("login attempts expired")
-    var userChoice = prompt("Would you like to create a new account? (yes/no): ");
-
-    if (userChoice === "yes"){
-        accountCreation();
-        accountCreationCheck = true;
+    if (accountCreationCheck === false){
+        var userChoice = prompt("Would you like to create a new account? (yes/no): ");
+        if (userChoice === "yes"){
+            accountCreation();
+            accountCreationCheck = true;
+        }else{
+            console.log("user did not want to create a new account");
+            accountCreationCheck = true;
+        } 
     }else{
         console.log("user did not want to create a new account");
-        accountCreationCheck = true;
-    }   
+    }
+  
 }
     
